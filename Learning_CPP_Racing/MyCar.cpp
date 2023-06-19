f8152318be53094fa1d27010d8305922cac2bb88
Stepa <korolyovstjepan@yandex.ru>
2023-06-19 19:59:21 +0700

    Regular update.

--- Learning_CPP_Racing/MyCar.cpp
+++ Learning_CPP_Racing/MyCar.cpp
@@ -8,13 +8,23 @@ AMyCar::AMyCar()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>("Body Of Car");
+	//Mesh = CreateDefaultSubobject<USkeletalMeshComponent>("Body Of Car");
 	CameraArm=CreateDefaultSubobject<USpringArmComponent>("Back Camera Arm");
+	CameraArm->SetupAttachment(GetMesh());
+	CameraArm->TargetOffset = FVector(0.f,0,200);
+	CameraArm->SetRelativeRotation(FRotator(-15,0,0));
+	CameraArm->TargetArmLength = 600;
+	CameraArm->bEnableCameraRotationLag = true;
+	CameraArm->CameraRotationLagSpeed = 8;
+	CameraArm->bInheritPitch = false;
+	CameraArm->bInheritRoll = false;
 	BackCamera=CreateDefaultSubobject<UCameraComponent>("Back Camera");
 	BackCamera->SetupAttachment(CameraArm);
 	BackCamera->bUsePawnControlRotation = false;
 	FrontCamera= CreateDefaultSubobject< UCameraComponent>("Front Camera");
-	SpeedPanelText= CreateDefaultSubobject<UTextRenderComponent>("Panel Text");;
+	FrontCamera->SetupAttachment(GetMesh());
+	SpeedPanelText= CreateDefaultSubobject<UTextRenderComponent>("Panel Text");
+	SpeedPanelText->SetupAttachment(GetMesh());
 }
 
 // Called when the game starts or when spawned
--- Learning_CPP_Racing/MyCar.h
+++ Learning_CPP_Racing/MyCar.h
@@ -6,18 +6,19 @@
 #include "GameFramework/Pawn.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "WheeledVehicle.h"
 #include "Components/TextRenderComponent.h"
 #include "MyCar.generated.h"
 
 UCLASS()
-class LEARNING_CPP_RACING_API AMyCar : public APawn
+class LEARNING_CPP_RACING_API AMyCar : public AWheeledVehicle //: public APawn
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this pawn's properties
 	AMyCar();
-	UPROPERTY(VisibleDefaultsOnly,BlueprintReadOnly) USkeletalMeshComponent* Mesh;
+	//UPROPERTY(VisibleDefaultsOnly,BlueprintReadOnly) USkeletalMeshComponent* Mesh;
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly) USpringArmComponent* CameraArm;
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly) UCameraComponent* BackCamera;
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly) UCameraComponent* FrontCamera;
